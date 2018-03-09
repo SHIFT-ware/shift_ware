7e8d0c222df9481d6b72f3ae4c7efeb8b5a11c1f
ko-he- <koheiyatsushiro@yahoo.co.jp>
2018-03-09 04:31:08 +0000

    Fix subject.content

--- Serverspec/spec/1-0001_Base/Advanced/1-0001-098_Ssh_spec.rb
+++ Serverspec/spec/1-0001_Base/Advanced/1-0001-098_Ssh_spec.rb
@@ -1,20 +1,21 @@
 describe "098-Ssh" do
   context "/etc/ssh/sshd_configの" do
-    subject{ file('/etc/ssh/sshd_config') }
+    subject(:sshd_config){ file('/etc/ssh/sshd_config') }
     setting = {true: "yes", false: "no"}
 
     permit_root_login  = setting[ property.dig(:ADVANCED, :sshd, :permit_root_login).to_s.to_sym ]
 
     it "PermitRootLoginが#{permit_root_login}であること", if: permit_root_login != nil do
-      is_expected.to match /^PasswordAuthentication\s+#{permit_root_login}\s*$/
+      expect(sshd_config.content).to match /^PermitRootLogin\s+#{permit_root_login}\s*/
     end
-
 
     password_auth = setting[ property.dig(:ADVANCED, :sshd, :password_auth).to_s.to_sym ]
     
     it "PasswordAuthenticationが#{password_auth}であること", if: password_auth != nil do
-      is_expected.to match /^PasswordAuthentication\s+#{password_auth}\s*$/
+      expect(sshd_config.content).to match /^PasswordAuthentication\s+#{password_auth}\s*$/
     end
   end
 end
+
+
 
