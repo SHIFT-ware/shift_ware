d72df10db7863fe493b390c25195c93ce3cc0cf0
yatsushiro kohei <koheiyatsushiro@yahoo.co.jp>
2018-03-20 14:47:38 +0900

    Common ver up ansble2.4 1 0001 base sshd (#24)
    * Use has_nested_keys
    
    * Add sshd test
    
    * Enable test if value is true
    
    * Fix subject.content

--- Serverspec/spec/1-0001_Base/Advanced/1-0001-098_Ssh_spec.rb
+++ Serverspec/spec/1-0001_Base/Advanced/1-0001-098_Ssh_spec.rb
@@ -1,19 +1,21 @@
-describe ("098-Ssh") do
-  begin
-    sshd = property[:ADVANCED][:sshd]
-  rescue NoMethodError
-    sshd = nil
+describe "098-Ssh" do
+  context "/etc/ssh/sshd_configの" do
+    subject(:sshd_config){ file('/etc/ssh/sshd_config') }
+    setting = {true: "yes", false: "no"}
+
+    permit_root_login  = setting[ property.dig(:ADVANCED, :sshd, :permit_root_login).to_s.to_sym ]
+
+    it "PermitRootLoginが#{permit_root_login}であること", if: permit_root_login != nil do
+      expect(sshd_config.content).to match /^PermitRootLogin\s+#{permit_root_login}\s*/
+    end
+
+    password_auth = setting[ property.dig(:ADVANCED, :sshd, :password_auth).to_s.to_sym ]
+    
+    it "PasswordAuthenticationが#{password_auth}であること", if: password_auth != nil do
+      expect(sshd_config.content).to match /^PasswordAuthentication\s+#{password_auth}\s*$/
+    end
   end
-
-  next if sshd == nil
-
-  describe file('/etc/ssh/sshd_config') do
-    describe ("rootで直接ssh接続できる設定になっていること"), :if => sshd[:permit_root_login] == TRUE do
-      its(:content) { should_not match /^PermitRootLogin\s+no\s*$/ }
-    end
-
-    describe ("password認証が可能なこと"), :if => sshd[:password_auth] == TRUE do
-      its(:content) { should_not match /^PasswordAuthentication\s+no\s*$/ }
-    end
-  end 
 end
+
+
+
