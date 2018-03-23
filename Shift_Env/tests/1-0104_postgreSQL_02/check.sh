721b6381b9f53d75b0d9a42657c947b4e648589a
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2018-03-23 16:45:13 +0900

    mod test

--- Shift_Env/tests/1-0104_postgreSQL_02/check.sh
+++ Shift_Env/tests/1-0104_postgreSQL_02/check.sh
@@ -2,7 +2,7 @@
 
 cd ${WORKSPACE}/shift
 
-grep -e "192.168.127.31" Shift_Log/Ansible.log | grep -e "changed=12"
+grep -e "192.168.127.31" Shift_Log/Ansible.log | grep -e "changed=7"
 result01=$?
 
 exit $result01
